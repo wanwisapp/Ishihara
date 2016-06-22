c935b7072027cfd281454bac547e06996ea0ebc3
James <Enkanpong1308@gmail.com>
2016-06-22 16:39:31 +0700

    latest

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -16,7 +16,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var answerTextField: UITextField!
     
-    var strAnswer:String = ""
+    var strAnswer:String = "0"
+    var intIndex:Int = 0
+    var arrayImage = ["ishihara_01.png","ishihara_01.png","ishihara_02.png","ishihara_03.png","ishihara_04.png","ishihara_05.png","ishihara_06.png","ishihara_07.png","ishihara_08.png","ishihara_09.png","ishihara_10.png"]
+    var myAnswer = ["3","5","6","12","29","45","74","42","0","0"]
+    var score:Int = 0
+    
+    
     
     
     
@@ -36,6 +42,26 @@ class ViewController: UIViewController {
         
         strAnswer = String(answerTextField.text)
         print("stranswer = \(strAnswer)")
+        
+        
+        
+        
+        intIndex += 1
+        print("intIndex ==> \(intIndex)")
+        
+        if (intIndex < 9) {
+            intIndex += 1
+        }else {
+            intIndex = 0
+        }
+        print ("intIndex ===> \(intIndex)")
+        
+        ishiharaImageView.image = UIImage(named: arrayImage[intIndex])
+        
+        if (strAnswer == myAnswer[intIndex]) {
+            score += 1
+            print("Score ==> \(score)")
+        }
         
     }
 
