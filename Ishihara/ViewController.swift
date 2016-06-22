c10523d869b69ff628cc084b1ce411c513d45d33
James <Enkanpong1308@gmail.com>
2016-06-22 14:38:40 +0700

    ได้แว้วววววว

--- Ishihara/ViewController.swift
+++ Ishihara/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ishiharaImageView: UIImageView!
+    
 
+    @IBOutlet weak var answerTextField: UITextField!
+    
+    var strAnswer:String = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +28,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    @IBAction func answerButton(sender: AnyObject) {
+        
+        strAnswer = String(answerTextField.text)
+        print("stranswer = \(strAnswer)")
+        
     }
 
 
